8a922def4c4cabe1b1ec8ddbd221846d54fab4ea
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-05-30 15:28:33 +0300

    created customer GradientContainer widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                const Color.fromARGB(255, 255, 94, 98),
-                const Color.fromARGB(255, 255, 195, 113),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const Center(
-            child: Text(
-              'Hello, World!',
-              style: TextStyle(
-                fontSize: 48,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 255, 94, 98),
+            const Color.fromARGB(255, 255, 195, 113),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello, Hitz!',
+          style: TextStyle(
+            fontSize: 48,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
