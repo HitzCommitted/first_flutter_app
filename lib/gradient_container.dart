10bd9d6ec966ae0f9d8e6a245779f7cdd1661bf0
HitzCommitted <147034456+HitzCommitted@users.noreply.github.com>
2025-06-04 09:12:50 +0300

    added dynamic being and end alignments

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:first_flutter_app/styled_text.dart';
 
+var beginAlignment = Alignment.topLeft;
+var endAlignment = Alignment.bottomRight;
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [
+          colors: const [
             Color.fromARGB(255, 255, 94, 98),
             Color.fromARGB(255, 255, 195, 113),
           ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+          begin: beginAlignment,
+          end: endAlignment,
         ),
       ),
       child: const Center(child: StyledText()),
